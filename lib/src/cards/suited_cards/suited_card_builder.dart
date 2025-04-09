3f1033a4bccc35430b5a360b91f035b2929235e3
jaboyc <jake@jlogical.com>
2025-04-09 19:42:03 -0400

    Remove playing_cards dependency and use open-source SVGs for cards.

--- lib/src/cards/suited_cards/suited_card_builder.dart
+++ lib/src/cards/suited_cards/suited_card_builder.dart
@@ -1,6 +1,6 @@
 import 'package:card_game/src/cards/suited_cards/suited_card.dart';
 import 'package:flutter/material.dart';
-import 'package:playing_cards/playing_cards.dart';
+import 'package:flutter_svg/svg.dart';
 
 /// A widget that renders a visual representation of a [SuitedCard].
 ///
@@ -12,51 +12,33 @@ class SuitedCardBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CardTheme(
-      data: CardThemeData(
-        margin: EdgeInsets.zero,
-        elevation: 0,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        border: Border.all(color: Colors.black, width: 2),
       ),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.white,
-          border: Border.all(color: Colors.black, width: 2),
-        ),
-        padding: EdgeInsets.all(1),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: PlayingCardView(
-            card: PlayingCard(getSuit(card), getValue(card)),
+      padding: EdgeInsets.all(1),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 2),
+          child: SvgPicture.asset(
+            'assets/${getValueName(card)}_of_${card.suit.name}.svg',
+            package: 'card_game',
+            width: 64,
+            height: 89,
           ),
         ),
       ),
     );
   }
 
-  Suit getSuit(SuitedCard card) => switch (card.suit) {
-        CardSuit.hearts => Suit.hearts,
-        CardSuit.diamonds => Suit.diamonds,
-        CardSuit.clubs => Suit.clubs,
-        CardSuit.spades => Suit.spades,
-      };
-
-  CardValue getValue(SuitedCard card) => switch (card.value) {
-        NumberSuitedCardValue(:final value) => switch (value) {
-            2 => CardValue.two,
-            3 => CardValue.three,
-            4 => CardValue.four,
-            5 => CardValue.five,
-            6 => CardValue.six,
-            7 => CardValue.seven,
-            8 => CardValue.eight,
-            9 => CardValue.nine,
-            10 => CardValue.ten,
-            _ => throw UnimplementedError(),
-          },
-        JackSuitedCardValue() => CardValue.jack,
-        QueenSuitedCardValue() => CardValue.queen,
-        KingSuitedCardValue() => CardValue.king,
-        AceSuitedCardValue() => CardValue.ace,
+  String getValueName(SuitedCard card) => switch (card.value) {
+        NumberSuitedCardValue(:final value) => value.toString(),
+        JackSuitedCardValue() => 'jack',
+        QueenSuitedCardValue() => 'queen',
+        KingSuitedCardValue() => 'king',
+        AceSuitedCardValue() => 'ace',
       };
 }
